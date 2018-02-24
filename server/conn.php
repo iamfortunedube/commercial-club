40744a858f7f3d1201db3c7b3f6bf4aadc481b47
KhayelihleS <kunenekhayelihle@gmail.com>
2018-02-25 00:05:42 +0200

    connection script

---
+++ server/conn.php
@@ -0,0 +1,10 @@
+<?php
+$servername="localhost";$username="root";$password="";$dbName="cc";
+$conn= new mysqli($servername,$username,$password,$dbName);
+if(!$conn)
+{
+echo" not connected";	
+}else{
+echo"connected";	
+}
+?>
