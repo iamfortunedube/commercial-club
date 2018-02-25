97c2f253c2c74eea11ff7dabd140f3b37b694fe9
Melusi <Melusimaphumulo@gmail.com>
2018-02-25 22:44:30 +0200

    working on login just fixed template

--- inc/loginContent.php
+++ inc/loginContent.php
@@ -4,9 +4,13 @@
                                 <div class="welcomeTitle">
                                         <h6>Login</h6>
                                 </div>
-                            <form class="welcomeContent">
-                                <input class="form-control" type="text" name="cellNumber" placeholder="Phone Number"/>
-                                <input class="form-control" type="password" name="password" placeholder="Password"/>
+
+                                <?php include("server/loginScript.php");?>
+                            <form id="login" class="welcomeContent" method="post" action="<?php echo $_SERVER["PHP_SELF"];?>">
+                                <input class="form-control" type="text" name="cellNumber" placeholder="Phone Number" value="<?php echo @$cellNo;?>"/>
+                                 <?php echo "<span style='color:red'>".@$errCellNumber."</span>";?>
+                                <input class="form-control" type="password" name="password" placeholder="Password" />
+                                 <?php echo "<span style='color:red'>".@$errPassword."</span>";?>
                                 <a href="forgotP.php" style="color: black; font-size:18px;" class="float-right">forgot password?</a>
                                 <input style="width:100%;" class="btn button-gold" type="submit" name="submit" value="Login"/>
                             </form>
