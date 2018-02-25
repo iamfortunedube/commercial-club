c0e555f3424b02f1e6d0bee693e49d411a236d34
KhayelihleS <kunenekhayelihle@gmail.com>
2018-02-25 18:05:44 +0200

    changes

--- inc/registerContent.php
+++ inc/registerContent.php
@@ -1,7 +1,8 @@
 <div class="row">   
     <div class="col-md-6">
-	
-	
+
+<?php include("server/register.php");?>
+    
         <div class="welcomeWrapper">
                 <div class="welcomeTitle">
                         <h6>Registration</h6>
@@ -9,31 +10,34 @@
                 <form id="reg" class="welcomeContent" method="post" action="<?php echo $_SERVER["PHP_SELF"];?>">
                     <div class="row">
                         <div class="col-md-6">
-                            <input class="form-control" type="text" name="fname" placeholder="Name"/>
-							<?php echo @$errMessage;?>
+                            <input class="form-control" require type="text" name="fname" placeholder="Name" value="<?php echo @$surname;?>" />
+                            <?php echo "<span style='color:red'>".@$errName."</span>";?>
 
                         </div>
                         <div class="col-md-6">
-                            <input class="form-control" type="text" name="sname" placeholder="Surame"/>
-						<?php echo @$errMessage;?>
+                            <input class="form-control" require type="text" name="sname" placeholder="Surame" value="<?php echo @$firstname;?>"/>
+                      
+                        <?php echo "<span style='color:red'>".@$errSurname."</span>";?>
                         </div>  
                     </div>
-                    <input class="form-control" type="text" name="cellNumber" placeholder="Phone Number"/>
-					<?php echo @$errMessage;?>
-                    <input class="form-control" type="text" name="cellNumber2" placeholder="Confirm Phone Number"/>
-					<?php echo @$errMessage;?>
-                    <input class="form-control" type="password" name="password" placeholder="Password"/>
-                     <?php echo @$errMessage;?>
-                    <input class="form-control" type="password" name="password2" placeholder="Confirm Password"/>
-					<?php echo @$errMessage;?>
-                    <input style="width:100%;" class="btn button-gold" type="submit" name="submit"value="Register"/>
-					<?php echo @$errMessage;?>
+                    <input class="form-control" require type="text" name="cellNumber" value="<?php echo @$cellNo;?>" placeholder="Phone Number"/>
+                    <?php echo "<span style='color:red'>".@$errCellNumber."</span>";?>
+                    <input class="form-control" require type="text" name="cellNumber2" value="<?php echo @$cellNo2;?>" placeholder="Confirm Phone Number"/>
+                    <?php echo "<span style='color:red'>".@$errCnumber."</span>";?>
+                    
+
+                    <input class="form-control" require type="password" name="password" value="<?php echo @$password;?>" placeholder="Password"/>
+                     <?php echo "<span style='color:red'>".@$errPassword."</span>";?>
+                    <input class="form-control" require type="password" name="password2" value="<?php echo @$password2;?>" placeholder="Confirm Password"/>
+					<?php echo "<span style='color:red'>".@$errCpassword."</span>";?>
+                    <input style="width:100%;" class="btn button-gold" require type="submit" name="submit"value="Register"/>
+					<?php echo "<span style='color:red'>".@$errMessage."</span>";?>
                 </form>
 
 							
         </div>
     </div>
-	<?php include("server/register.php");?> 
+	
 
     <div class="col-md-6">
         <div class="welcomeWrapper">
