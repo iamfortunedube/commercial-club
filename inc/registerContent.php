2b3ebe834c5e063d89b7574839a21d471f85a353
KhayelihleS <kunenekhayelihle@gmail.com>
2018-02-25 13:17:04 +0200

    update register

--- inc/registerContent.php
+++ inc/registerContent.php
@@ -1,26 +1,40 @@
 <div class="row">   
     <div class="col-md-6">
+	
+	
         <div class="welcomeWrapper">
                 <div class="welcomeTitle">
                         <h6>Registration</h6>
                 </div>
-                <form class="welcomeContent">
+                <form class="welcomeContent" method="post" action="<?php echo $_SERVER["PHP_SELF"];?>">
                     <div class="row">
                         <div class="col-md-6">
                             <input class="form-control" type="text" name="fname" placeholder="Name"/>
+							<?php echo @$errMessage;?>
+
                         </div>
                         <div class="col-md-6">
                             <input class="form-control" type="text" name="sname" placeholder="Surame"/>
+						<?php echo @$errMessage;?>
                         </div>  
                     </div>
-                    <input class="form-control" type="text" name="cellNumber" placeholder="Phone Number"/>
-                    <input class="form-control" type="text" name="cellNumber" placeholder="Confirm Phone Number"/>
+                    <input class="form-control" type="number" name="cellNumber" placeholder="Phone Number"/>
+					<?php echo @$errMessage;?>
+                    <input class="form-control" type="number" name="cellNumber2" placeholder="Confirm Phone Number"/>
+					<?php echo @$errMessage;?>
                     <input class="form-control" type="password" name="password" placeholder="Password"/>
-                    <input class="form-control" type="password" name="password" placeholder="Confirm Password"/>
+					<?php echo @$errMessage;?>
+                    <input class="form-control" type="password" name="password2" placeholder="Confirm Password"/>
+					<?php echo @$errMessage;?>
                     <input style="width:100%;" class="btn btn-success" type="submit" name="submit"value="Register"/>
+					<?php echo @$errMessage;?>
                 </form>
+							
         </div>
     </div>
+	<?php include("server/register.php");?> 
+	
+	
     <div class="col-md-6">
         <div class="welcomeWrapper">
                 <div class="welcomeTitle">
---
+++ server/register.php
@@ -0,0 +1,115 @@
+<?php
+require "conn.php";
+$name=$surname=$cellNo=$password=$password2=$cellNo2=$errMessage=$vCode=$bank_name=$account_holder="";
+$rCode=$status=$uniCode=$accNum=0;
+if($_SERVER["REQUEST_METHOD"]=="POST")
+	{
+		$surname=$_POST["sname"];
+	   	$firstname=$_POST["fname"];	
+		//$vCode=$_POST["vCode"];
+		$cellNo=$_POST["cellNumber"];
+		$cellNo2=$_POST["cellNumber2"];
+		$password=$_POST["password"];
+		$password2=$_POST["password2"];
+	     if(isset($_POST["submit"]))
+    {
+	$sql="select p_number from users where  p_number=\"$cellNo\";";
+		
+		
+		$result=$conn->query($sql);
+		if($result->num_rows>0)
+		{
+			$errMessage="user already have an account";
+		}
+	
+		 if($result->num_rows<=0) {
+		 if (empty($_POST["surname"])) 
+		      { 
+		       $errMessage="Enter your surname *"; 
+			  }else if(empty($_POST["firstname"]))
+			  {
+                $errMessage="Enter your Name *";
+                echo $errMessage;				
+			  }else if(empty($_POST["cellNo"]))
+			  {
+				  $errMessage="Enter your cell Number *";   
+			  }else if(empty($_POST["cellNo2"]))
+			  {
+				 $errMessage="Re-Enter your cell Number *";  
+			  }else if(empty($_POST["password"]))
+			  {
+				  $errMessage="Enter your password *";  
+			  }else if(empty($_POST["password2"]))
+			  {
+				$errMessage="Re-Enter your password *";  
+			  }
+
+			 if($cellNo==$cellNo2)
+			     {
+			 if($password==$password2)
+				{
+			$sql="insert into users values(\"$firstname\",\"$surname\",\"$cellNo\",\"$password\",\"$rCode\",\"$vCode\",\"$status\",\"$bank_name\",\"$uniCode\",\"$account_holder\",\"$accNum\");";
+					 if($conn->query($sql))
+	                  {
+		               echo" Inserted";   
+	                   }else{
+		             $errMessage= " Not Inserted".$conn->error; 
+	                  }
+				}else{
+					 $errMessage="Passwords does not metch";
+				}
+				 }else{
+					 $errMessage="Phone Number does not metch"; 
+				 }
+			  }
+		 
+			//check empty fields
+			/*if (empty($_POST["surname"])) 
+		      { 
+		       $ErrMessage="Enter your surname *"; 
+			  }else if(empty($_POST["firstname"]))
+			  {
+                $ErrMessage="Enter your Name *"; 
+			  }else if(empty($_POST["cellNo"]))
+			  {
+				  $ErrMessage="Enter your cell Number *";   
+			  }else if(empty($_POST["cellNo2"]))
+			  {
+				 $ErrMessage="Re-Enter your cell Number *";  
+			  }else if(empty($_POST["password"]))
+			  {
+				  $ErrMessage="Enter your password *";  
+			  }else if(empty($_POST["password2"]))
+			  {
+				$ErrMessage="Re-Enter your password *";  
+			  }
+			  else if($cellNo==$cellNo2)
+			     {
+				if($password==$password2)
+				{
+					$sql="insert into users (fname,lname,p_number,password,ref_code,vCode,status,bank_name,universal_code,account_holder,account_number)values(\"$fname\",\"$lname\",\"$p_number\",\"$password\",\"$rCode\",\"$vCode\",\"$status\",\"$bank_name\",\"$uniCode\",\"$account_holder\",\"$accNum\");";
+					 if($conn->query($sql))
+	                  {
+		               echo" Inserted";   
+	                   }else{
+		              echo" Not Inserted".$conn->error; 
+	                  }
+				}else{
+	          echo"Passwords does not metch";
+				}
+			}else{
+	         echo"Phone Number does not metch";
+			}*/			
+		
+	}
+	}
+	
+	  function test_input($data)
+   {
+	$data=trim($data);
+	$data=stripslashes($data);
+	$data=htmlspecialchars($data);
+	return $data;
+}
+$conn->close();	
+?>
