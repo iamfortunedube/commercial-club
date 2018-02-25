016e4cb7410459129b36b4f6838ca5b989ebeee6
spha1234 <sphafortunedube@outlook.com>
2018-02-25 20:53:27 +0200

    Tabs

--- inc/sideBar.php
+++ inc/sideBar.php
@@ -3,17 +3,18 @@
         <li>
             <div class="userProfile">
                 <div class="imageLeft">
-                        <img src="assets/avatar.png" width="70" height="70" alt="image" />
-                    </div>
-                    <div class="userWelcome">
-                        <p class="welcomeM">Welcome <b>Guest</b></p>
-                        <p class="messageTrans">Kindly login to start Transacting</p>
+                    <img src="assets/avatar.png" width="70" height="70" alt="image" />
+                </div>
+                <div class="userWelcome">
+                    <p class="welcomeM">Welcome <b>Guest</b></p>
+                    <p class="messageTrans">Kindly login to start Transacting</p>
                 </div>
             </div>
         </li>
         <li><a id="home" class="active" href="index.php"> <i class="fas fa-home"></i> Home</a></li>
         <li><a id="login" href="login.php"><i class="fas fa-sign-in-alt"></i> Login</a></li>
         <li><a id="register" href="register.php"><i class="fas fa-edit"></i> Register</a></li>
+        <li><a id="profile-edit" href="#"><i class="fas fa-user"></i> Profile</a></li>
         <li><a id="donations" href="donate.php"><i class="fas fa-money-bill-alt"></i> Donations</a></li>
         <li><a id="claims" href="claims.php"><i class="fas fa-credit-card"></i> Claims</a></li>
         <li><a id="sign-out" href="#"><i class="fas fa-sign-out-alt"></i> Sign out</a></li>
