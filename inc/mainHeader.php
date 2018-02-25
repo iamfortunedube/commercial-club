a81a181335d120816998c52206c18c5c85763dc7
KhayelihleS <kunenekhayelihle@gmail.com>
2018-02-25 15:16:02 +0200

    changes

--- inc/mainHeader.php
+++ inc/mainHeader.php
@@ -1,9 +1,11 @@
 <div class="mainheader">
         <header>
             <div class="leftHeader">
-                <img src="#" alt="logo" hidden/>
-                <h3>Logo</h3>
+                <img src="assets/logoCC.png" width="50" height="50" alt="logo"/>
             </div>
+            <center>
+                <img id="namelogo" src="assets/nameLogo.png" alt="logo"/>
+            </center>
             <div class="rightHeader">
                 +27 31 123 4567
             </div>
