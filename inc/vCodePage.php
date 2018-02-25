0f2adb9242328a11e8a9ae91e5cfabb47ff18599
spha1234 <sphafortunedube@outlook.com>
2018-02-25 15:52:08 +0200

    nothing

--- inc/vCodePage.php
+++ inc/vCodePage.php
@@ -4,14 +4,13 @@
 	
         <div class="welcomeWrapper">
                 <div class="welcomeTitle">
-                        <h6>Registration</h6>
+                    <h6>Registration</h6>
                 </div>
 				<form id="vCode" class="welcomeContent" method="post" action="<?php echo $_SERVER["PHP_SELF"];?>">
                     <div class="row">
                         <div class="col-md-9">
                             <input class="form-control" type="text" name="vCode" placeholder="Verification Code"/>
 							<?php echo @$errMessage;?>
-
                         </div>
                         <div class="col-md-3">
                           <input style="width:100%;" class="btn button-gold" type="submit" name="submit"value="Confirm" id="hideVer"/>
