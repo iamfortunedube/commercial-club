d1cca470f9a5dd19513b845fb02d2b3f3aa1db3d
Melusi <Melusimaphumulo@gmail.com>
2018-02-26 09:50:59 +0200

    support feature set when loggedIn

--- inc/support.php
+++ inc/support.php
@@ -1,11 +1,20 @@
-<div class="support">
+<?php
+if(isset($_SESSION['u_id'])){
+    echo '
+    <div class="support">
         <a href="#" id="show"><div class="header">Need Help?</div></a>
         <form id="form">
             <div class="form-group">
                 <textarea placeholder="Comment" class="form-control" row="10" type="text" name="comment" style="resize: none;"></textarea>
             </div>
-            <div class="form-group">
-                    <input id="send" class="btn button-gold" type="submit" name="submit" value="Send">
-            </div>
-        </form>
-</div>
+                <div class="form-group">
+                        <input id="send" class="btn button-gold" type="submit" name="submit" value="Send">
+                </div>
+            </form>
+    </div>
+    
+         ';
+}
+
+
+?>
