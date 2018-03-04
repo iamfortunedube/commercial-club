2afca2ae4b4261eca7c04a8c278d4459a44826ba
MelB <Mellusimaphumulo@gmail.om>
2018-03-04 17:47:26 +0100

    removed active

--- index.php
+++ index.php
@@ -4,3 +4,9 @@
   if(isset($_SESSION["u_id"])){ header("location:dashboard.php"); } ?>
   <?php include("inc/home.php");?>
 <?php include("inc/overallFooter.php");?>
+<script>
+      $(document).ready(function(){
+        $('.sidebar-menu ul li a').removeClass('active');
+        $('.sidebar-menu ul li #home').addClass('active');
+      });
+</script>  
