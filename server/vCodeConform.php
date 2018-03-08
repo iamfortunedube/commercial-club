a6b424ab898e9c3c9bd7cc13195c2d8af851c157
MelB <Mellusimaphumulo@gmail.om>
2018-03-08 14:52:57 +0100

    commercial club

--- server/vCodeConform.php
+++ server/vCodeConform.php
@@ -55,8 +55,8 @@
                 $row = $getResults->fetch_assoc();
                 
                 $uniCode = $row['universal_code'];
-                $sql = "insert into users values('',\"$firstname\",\"$surname\",\"$cellNo\",\"$password\",\"$refferalNo\",0,\"$bank_name\",\"$uniCode\",\"$bank_branch\",\"$account_holder\",\"$accNum\",'assets/avatar.png');"; 
-                $sqlRef = "insert into referals values('',\"$refferalNo\",\"$cellNo\")";
+                $sql = "insert into users values('',\"$firstname\",\"$surname\",\"$cellNo\",\"$password\",\"$refferalNo\",0,\"$bank_name\",\"$uniCode\",\"$bank_branch\",\"$account_holder\",\"$accNum\",'assets/avatar.png',0);"; 
+                $sqlRef = "insert into referals values('',\"$refferalNo\",\"$cellNo\",0,0)";
                 $conn->query($sqlRef);
                 if($conn->query($sql)){
 
